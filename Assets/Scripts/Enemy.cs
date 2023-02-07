86747079d7078f0e7b01163b9fbc0a11bd67b700
Elias Kupeczki <github@kunoxy.ch>
2023-02-07 13:01:22 +0100

    update

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,26 +5,17 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public new Rigidbody2D rigidbody;
-    // Start is called before the first frame update
     void Start()
     {
-
     }
-
-    // Update is called once per frame
     void Update()
     {
-
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "ball")
         {
-            //add upwards force
-            print("skjdf");
             rigidbody.AddForce(new Vector2(0, -1000));
-
         }
-
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,37 +5,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public new Rigidbody2D rigidbody;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKey(KeyCode.A))
         {
             transform.Translate(Vector3.left * Time.deltaTime * 10);
-
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Translate(Vector3.right * Time.deltaTime * 10);
-
         }
-
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "ball")
         {
-            //add upwards force
-            //rigidbody.AddForce(new Vector2(0, 1000));
-
+            rigidbody.AddForce(new Vector2(0, 1000));
         }
-
-
     }
 }
